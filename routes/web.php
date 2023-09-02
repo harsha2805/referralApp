08830d90b8853f6abfc2878eb06e19338dd047de
Harsha <harshapeace123@gmail.com>
2023-09-02 16:17:18 +0530

    Added Share Buttons

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ Route::get('/', [GamerController::class, 'signup'])->name('signup');
 Route::get('/referral/{referralKey}', [GamerController::class, 'signup'])->name('signup.referral');
 Route::post('/signup', [GamerController::class, 'processSignupForm'])->name('signup.submit');
 Route::view('/thank-you', 'thank-you')->name('signup.thankyou');
-
-
 /**Route::get('referral/{referral_key}', [GamerController::class, 'data'])
     ->middleware('VerifyReferralKey')
     ->name('referral.link');
